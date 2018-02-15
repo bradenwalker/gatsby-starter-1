07743295db4dd1eee386e7abddba4abeab82198f
Braden Walker <wwbraden@gmail.com>
2018-02-15 11:33:58 -0600

    move files

---
+++ .cache/async-requires.js
@@ -0,0 +1,26 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/bwalker/Downloads/art/site/gatsby-starter-1/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/bwalker/Downloads/art/site/gatsby-starter-1/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/bwalker/Downloads/art/site/gatsby-starter-1/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/404.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/layout-index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/page-2.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/layout-index.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/layouts/index.js")
+}
---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/bwalker/Downloads/art/site/gatsby-starter-1/src/layouts/index.js"
+  import data from "/Users/bwalker/Downloads/art/site/gatsby-starter-1/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
