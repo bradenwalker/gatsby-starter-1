96973688da2e2e91bdad6a652193be284310f272
Braden Walker <wwbraden@gmail.com>
2018-02-15 11:55:33 -0600

    add plot-synopsis.js

---
+++ src/pages/plot-synopsis.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const PlotPage = () => (
+  <div>
+    <h1>Plot Synopsis</h1>
+    <p>Coming soon ...</p>
+    <Link to="/">Home</Link>
+  </div>
+)
+
+export default PlotPage
